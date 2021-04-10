5cab99722a403f79bbddfa5eea8df0e739a236e9
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2021-04-10 11:03:13 -0400

    a

--- src/ProjectHelper.ts
+++ src/ProjectHelper.ts
@@ -4,19 +4,59 @@ import { DotNetProject } from "./DotNetProject";
 export class ProjectHelper {
     constructor() { }
 
-    public static createProject(exactSlnText: string, 
-                                slnAbsolutePath: string, 
-                                slnDisplayName: string)
+    private static position: number = 0;
+
+    public static createProject(exactSlnText: string,
+        slnAbsolutePath: string,
+        slnDisplayName: string)
         : DotNetProject {
 
-            if(!exactSlnText) {
-                throw new Error(`Argument passed to ${ProjectHelper.createProject.name}`);
+        if (!exactSlnText) {
+            throw new Error(`Argument passed to ${ProjectHelper.createProject.name}`);
+        }
+        if (!slnAbsolutePath) {
+            throw new Error(`Argument passed to ${ProjectHelper.createProject.name}`);
+        }
+        if (!slnDisplayName) {
+            throw new Error(`Argument passed to ${ProjectHelper.createProject.name}`);
+        }
+
+        this.position = 0;
+
+        let idOne: string = this.extractIdOne(exactSlnText);
+    }
+
+    private static extractIdOne(exactSlnText: string): string {
+        let idOne: string = "";
+        let finished: boolean = false;
+
+        while (!finished && _position < exactSlnText.Length) {
+            char currentChar = exactSlnText[_position];
+
+            switch (currentChar) {
+                case '{':
+                    _position++;
+                    if (_position < exactSlnText.Length) {
+                        currentChar = exactSlnText[_position];
+                    }
+
+                    while (currentChar != '}') {
+                        idOne.Append(currentChar);
+                        _position++;
+
+                        if (_position < exactSlnText.Length) {
+                            currentChar = exactSlnText[_position];
+                        }
+                    }
+
+                    finished = true;
+                    break;
+                default:
+                    _position++;
+                    break;
             }
-            if(!slnAbsolutePath) {
-                throw new Error(`Argument passed to ${ProjectHelper.createProject.name}`);
-            }
-            if(!slnDisplayName) {
-                throw new Error(`Argument passed to ${ProjectHelper.createProject.name}`);
-            }
+        }
+
+        return Guid.Parse(idOne.ToString());
     }
 }
