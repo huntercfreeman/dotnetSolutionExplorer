c3c394f6f6b9f90cbd82268128d79e1de43a342e
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2021-04-10 19:41:32 -0400

    a

---
+++ src/CopyState.ts
@@ -0,0 +1,41 @@
+export class CopyState {
+    constructor() {}
+
+    private absolutePath: string | undefined;
+    private isCutActive: boolean = false;
+
+    public copy(absolutePath: string) {
+        this.isCutActive = false;
+
+        this.absolutePath = absolutePath;
+    }
+
+    public cut(absolutePath: string) {
+        this.absolutePath = absolutePath;
+
+        this.isCutActive = true;
+    }
+
+    public readClipboard(): any | undefined {
+        if(!this.absolutePath) {
+            return undefined;
+        }
+
+        if(!this.isCutActive) {
+            return {
+                "absolutePath": this.absolutePath,
+                "wasCut": false
+            };
+        }
+
+        let temporary = this.absolutePath;
+
+        this.absolutePath = undefined;
+        this.isCutActive = false;
+        
+        return {
+            "absolutePath": this.absolutePath,
+            "wasCut": true
+        };
+    }
+}
