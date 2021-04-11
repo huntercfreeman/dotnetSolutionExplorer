23498b3269609460307dd03d6b03a3694e4aa000
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2021-04-10 22:18:08 -0400

    Changes

--- src/DotNetFileCs.ts
+++ src/DotNetFileCs.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import { DotNetFile } from './DotNetFile';
 
-//sanity check
 export class DotNetFileCs extends DotNetFile {
     private constructor(
         public readonly absolutePath: string,
