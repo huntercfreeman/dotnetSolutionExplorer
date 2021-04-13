b2cc7477a89da59791083e386be883c94797a84b
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2021-04-13 12:59:33 -0400

    a

--- src/DotNetProjectReferenceList.ts
+++ src/DotNetProjectReferenceList.ts
@@ -6,6 +6,7 @@ import { DotNetPathHelper } from './DotNetPathHelper';
 import { ProjectHelper } from './ProjectHelper';
 const fs = require('fs');
 import { DotNetProjectDependencies } from './DotNetProjectDependencies';
+import { DotNetProjectReference } from './DotNetProjectReference';
 
 
 export class DotNetProjectReferenceList extends DotNetFile {
@@ -80,45 +81,3 @@ export class DotNetProjectReferenceList extends DotNetFile {
     }
 }
 
-export class DotNetProjectReference extends DotNetFile {
-    private constructor(
-        public readonly absolutePath: string,
-        public readonly filename: string,
-        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-        public readonly parent: DotNetFile
-    ) {
-        super(absolutePath, filename, collapsibleState, DotNetFileKind.txt, parent);
-
-        let uri: vscode.Uri = vscode.Uri.parse(absolutePath);
-
-        this.iconPath = {
-            light: path.join(__filename, '..', '..', 'resources', 'light', 'fileTxtIcon.svg'),
-            dark: path.join(__filename, '..', '..', 'resources', 'dark', 'fileTxtIcon.svg')
-        };
-
-        this.command = {
-            "command": "dotnet-solution-explorer.openFile",
-            "title": "open",
-            "arguments": [uri]
-        };
-    }
-
-    public static async createAsync(absolutePath: string, filename: string, parent: DotNetFile): Promise<DotNetFile> {
-        return new DotNetProjectReference(absolutePath, filename, vscode.TreeItemCollapsibleState.None, parent);
-    }
-
-    public async getChildren(): Promise<DotNetFile[]> {
-        if (this.children) {
-            return Promise.resolve(this.children);
-        }
-        else {
-            this.children = [];
-
-            return Promise.resolve(this.children);
-        }
-    }
-
-    public tryFosterChildren(): Promise<void> {
-        return Promise.resolve();
-    }
-}
