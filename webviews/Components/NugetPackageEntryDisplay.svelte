52d36ba959633ab98f431af1cad4752089442368
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-06-03 13:03:15 -0400

    Expansion for nuget packages more detail

--- webviews/Components/NugetPackageEntryDisplay.svelte
+++ webviews/Components/NugetPackageEntryDisplay.svelte
@@ -3,12 +3,30 @@
 
 	export let entry;
 
+	let expandContents = false;
+
     function addNugetPackage() {
 		tsVscode.postMessage({
 			command: "addNugetPackage",
             title: entry.title
 		});
 	}
+
+	function toggleExpand() {
+		expandContents = !expandContents;
+	}
 </script>
 
-<div on:click="{addNugetPackage}" class="dnse_nuget-package-entry">{entry.title} +</div>
+<div on:click="{toggleExpand}" class="dnse_nuget-package-entry">{entry.title} +</div>
+
+{#if expandContents}
+	<div style="margin-left: 15px;">
+		<button on:click="{addNugetPackage}">
+			Add Nuget Package
+		</button>
+
+		<div>
+			<pre>{JSON.stringify(entry, null, 2)}</pre>
+		</div>
+	</div>
+{/if}
